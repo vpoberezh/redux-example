c9f05b3473638449c3d547638e5ce4ddb4008e88
Vadym P <vadym.poz@inx.co>
2022-01-30 00:35:10 +0200

    update

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import s from './index.module.scss';
+import classNames from 'classnames';
+import numeral from 'numeral';
+
+interface Props {
+    title?: string;
+    children: React.ReactNode;
+    total?: number;
+}
+
+export const Layout: React.FC<Props> = ({ children, title, total }) => {
+    return (
+        <div className={classNames(s.root)}>
+            <div className={s.header}>
+                <div className={s.menu}>
+                    <a className={s.link} href="/">
+                        Simple
+                    </a>
+                    <a className={s.link} href="/#/redux">
+                        Redux
+                    </a>
+                </div>
+                <div>{numeral(total).format('0,0[.]00 $')}</div>
+            </div>
+            <h1 className={s.title}>{title}</h1>
+            <div className={s.content}>{children}</div>
+            <div className={s.footer}>
+                <div>{title}</div>
+                <div>{numeral(total).format('0,0[.]00 $')}</div>
+            </div>
+        </div>
+    );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import numeral from 'numeral';
 import s from './index.module.scss';
 import { CoinRow } from './coin-row';
 import { CoinList } from '../consts';
+import { Layout } from '../components/layout';
 
 export const CustomPage: React.FC = () => {
     const [allSum, setAllSum] = useState<{ [key: string]: number }>({});
@@ -22,7 +23,7 @@ export const CustomPage: React.FC = () => {
     }, [allSum]);
 
     return (
-        <div className={s.root}>
+        <Layout title="Simple Wallet" total={total}>
             <div className={s.header}>
                 <div>Coin</div>
                 <div>$ Price</div>
@@ -35,6 +36,6 @@ export const CustomPage: React.FC = () => {
                 ))}
             </div>
             <div className={s.footer}>{`Total: ${numeral(total).format('0,0[.]00 $')}`}</div>
-        </div>
+        </Layout>
     );
 };
