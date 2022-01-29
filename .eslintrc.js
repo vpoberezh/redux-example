2c8c221d72338bb57032dd34c0f4e49dbb08f868
Vadym P <vadym.poz@inx.co>
2022-01-29 23:12:25 +0200

    stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,75 @@
 module.exports = {
-    extends: ['varp'],
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'prettier',
+    ],
+    plugins: ['@typescript-eslint', 'react-hooks', 'prettier'],
     rules: {
-        /* Additional rules */
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+        'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+        'react-hooks/rules-of-hooks': 'error',
+        'import/no-default-export': 'error',
+        'import/named': 'off',
+        'import/order': ['error', { groups: ['external'], 'newlines-between': 'always' }],
+        'import/first': 'error',
+        'import/no-named-as-default': 'off',
+        'react/prop-types': 'off',
+        'react/display-name': 'off',
+        'react/no-unescaped-entities': 'off',
+        camelcase: 'off',
+        '@typescript-eslint/camelcase': 'off',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', { args: 'after-used', ignoreRestSiblings: true }],
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-use-before-define': [
+            'error',
+            {
+                functions: false,
+                classes: true,
+                variables: true,
+                typedefs: true,
+            },
+        ],
+        '@typescript-eslint/no-empty-interface': ['error', { allowSingleExtends: true }],
+        '@typescript-eslint/no-useless-constructor': 'error',
+        'no-explicit-any': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'lines-between-class-members': 'error',
+        'import/order': 'off',
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+        'import/resolver': {
+            node: {
+                moduleDirectory: ['node_modules'],
+            },
+            typescript: {
+                project: './',
+            },
+            alias: {
+                map: [['src', './src']],
+                extensions: ['.ts', '.tsx', '.js'],
+            },
+        },
     },
 };
